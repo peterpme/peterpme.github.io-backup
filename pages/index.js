f75c3b1f1356b797f8a1aeb85f2f726c393af0d0
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-01-26 09:56:47 -0800

    add a few more things

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import facebook from '../images/facebook.svg'
-import twitter from '../images/twitter.svg'
-import github from '../images/github.svg'
-import medium from '../images/medium.svg'
+import React from 'react';
+import facebook from '../images/facebook.svg';
+import twitter from '../images/twitter.svg';
+import github from '../images/github.svg';
+import medium from '../images/medium.svg';
 
 var config = {
-  image: 'https://res.cloudinary.com/peterpme/image/upload/f_auto,w_200,h_200,g_center,c_fill/v1461435483/peter-piekarczyk-headshot.jpg',
+  image:
+    'https://res.cloudinary.com/peterpme/image/upload/f_auto,w_200,h_200,g_center,c_fill/v1461435483/peter-piekarczyk-headshot.jpg',
   name: 'Peter Piekarczyk',
-  title: 'Technical Cofounder, Orchard',
-}
+  title: 'Technical Cofounder, Orchard'
+};
 
 var socialLinks = [
   {
@@ -26,7 +27,7 @@ var socialLinks = [
     url: 'https://medium.com/@peterpme/',
     icon: medium
   }
-]
+];
 
 var speaking = [
   {
@@ -38,7 +39,8 @@ var speaking = [
     label: '2015 JSConf - Backbone & React - Building a Hybrid Application'
   },
   {
-    url: 'https://speakerdeck.com/peterpme/what-the-flux-a-quick-primer-on-react-and-flux',
+    url:
+      'https://speakerdeck.com/peterpme/what-the-flux-a-quick-primer-on-react-and-flux',
     label: '2015 Code N Cans @ Braintree - What The Flux? React & Flux Primer'
   },
   {
@@ -46,71 +48,107 @@ var speaking = [
     label: '2016 Node Foundation - Future of High Performance Node'
   },
   {
+    url: 'https://realm.io/blog/react-native-radio-realm/',
+    label: 'React Native Radio 24: Realm React Native'
+  },
+  {
     url: '#',
     label: '2016 React Remote Conf - Moving Practically'
   },
   {
-    url: 'https://speakerdeck.com/peterpme/things-i-wish-i-knew-about-react-native-when-i-started',
-    label: '2017 RN Chicago - Things I Wish I Knew About React Native When I Started'
+    url:
+      'https://speakerdeck.com/peterpme/things-i-wish-i-knew-about-react-native-when-i-started',
+    label:
+      '2017 RN Chicago - Things I Wish I Knew About React Native When I Started'
   },
   {
     url: '#',
-    label: '2017 Node.js Chicago - Dealing with Stress and Anxiety as a Developer',
+    label:
+      '2017 Node.js Chicago - Dealing with Stress and Anxiety as a Developer'
   },
   {
     url: 'https://www.youtube.com/watch?v=8XrCfsR7GoU',
-    label: '2017 DevopsDays - Stress & Anxiety as a developer'
+    label: '2017 DevopsDays - Stress & Anxiety as a Developer'
+  },
+  {
+    url:
+      'https://crosscuttingconcerns.com/Podcast-050-Peter-Piekarczyk-on-Burnout',
+    label: 'Burnout, Stress & Anciety as a Developer'
   }
-]
+];
 
-const Header = ({
-  image,
-  name,
-  title
-}) => (
-  <header className='Header'>
-    <figure className='Header-figure'>
-      <img className='Header-image' src={image} alt={`Photo of ${name}`} />
+const Header = ({ image, name, title }) => (
+  <header className="Header">
+    <figure className="Header-figure">
+      <img className="Header-image" src={image} alt={`Photo of ${name}`} />
       <figcaption>
-        <h1 className='Header-name'>{name}</h1>
-        <span className='Header-title'>{title}</span>
+        <h1 className="Header-name">{name}</h1>
+        <span className="Header-title">{title}</span>
       </figcaption>
     </figure>
   </header>
-)
+);
 
 export default class Index extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='Container'>
+      <div className="Container">
         <Header image={config.image} name={config.name} title={config.title} />
-        <ul className='Social'>
-          {socialLinks.map((link) => (
-            <li className='Social-item' key={link.label}>
+        <ul className="Social">
+          {socialLinks.map(link => (
+            <li className="Social-item" key={link.label}>
               <a className="Link" href={link.url} title={link.label}>
                 <img src={link.icon} width={36} height={36} alt={link.label} />
               </a>
             </li>
           ))}
         </ul>
-        <p className='About'>
-          I'm a Javascript Engineer. I love building complex applications in the most elegant of ways.<br /><br/>
-          I'm currently working on <a className="Link" target="_blank" href="https://www.orchard.ai">Orchard</a>. Orchard helps you stay in touch with the people that matter the most. We use machine learning and some relationship science to help you reconnect with old colleagues, friends and business partners.<br /><br/>
-          When it comes to building things, I like to move rapidly. I utilize technologies such as React & React Native, Expo, Apollo, GraphQL and ReasonML.<br/><br/>
-          If you'd like to get in touch with me, shoot me an <a className="Link" href="mailto:contact@peterp.me">email</a>
+        <p className="About">
+          I'm a Javascript Engineer. I love building complex applications in the
+          most elegant of ways.<br />
+          <br />
+          I'm currently working on{' '}
+          <a
+            title="Orchard: Manage your personal and professional network"
+            className="Link"
+            target="_blank"
+            href="https://www.orchard.ai"
+          >
+            Orchard
+          </a>. Orchard helps you stay in touch with the people that matter the
+          most. We use machine learning and some relationship science to help
+          you reconnect with old colleagues, friends and business partners.<br />
+          <br />
+          When it comes to building things, I like to move rapidly. I utilize
+          technologies such as React & React Native, Expo, Apollo, GraphQL and
+          ReasonML.<br />
+          <br />
+          If you'd like to get in touch with me, shoot me an{' '}
+          <a className="Link" href="mailto:contact@peterp.me">
+            email
+          </a>
         </p>
-        <br/>
-        <h2 className='About-heading'>Speaking:</h2>
-        <ul className='Speaking'>
+        <br />
+        <h2 className="About-heading">Speaking:</h2>
+        <ul className="Speaking">
           {speaking.reverse().map((event, index) => (
-            <li className='About-heading' style={{ textAlign: 'left' }} key={index}>
-              <a target="_blank" className="Link" href={event.url} title={event.label}>
+            <li
+              className="About-heading"
+              style={{ textAlign: 'left' }}
+              key={index}
+            >
+              <a
+                target="_blank"
+                className="Link"
+                href={event.url}
+                title={event.label}
+              >
                 {event.label}
               </a>
             </li>
           ))}
         </ul>
       </div>
-    )
+    );
   }
 }
